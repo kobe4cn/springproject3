d89f6154bd8e70f18fcdb2e71f5207d8760d877a
kobe4cn <kevin.yang.xgz@gmail.com>
2023-03-19 13:24:46 +0800

    add new line for testing

--- backend/src/main/java/com/example/springproject3/Springproject3Application.java
+++ backend/src/main/java/com/example/springproject3/Springproject3Application.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Random;
 
+
 @SpringBootApplication
 @RestController
 public class Springproject3Application {
