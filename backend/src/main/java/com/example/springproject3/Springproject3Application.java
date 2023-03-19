06cf793f356f81fde1d5913da07a2854e4a9fb57
kobe4cn <kevin.yang.xgz@gmail.com>
2023-03-19 21:45:41 +0800

    测试自动化发布之后的，aws部署文件更新到git上

--- backend/src/main/java/com/example/springproject3/Springproject3Application.java
+++ backend/src/main/java/com/example/springproject3/Springproject3Application.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Random;
 import java.util.UUID;
 
+
 @SpringBootApplication
 @RestController
 public class Springproject3Application {
